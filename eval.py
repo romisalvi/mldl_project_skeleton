163a03d37f39cba22cef9b643422ae72dc1e0b2a
rsalvi100 <162099174+romisalvi@users.noreply.github.com>
2025-03-18 13:28:33 +0100

    Update eval.py

---
+++ eval.py
@@ -0,0 +1,25 @@
+# Validation loop
+def validate(model, val_loader, criterion):
+    model.eval()
+    val_loss = 0
+    size=len(val_loader)
+
+    correct, total = 0, 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(val_loader):
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            val_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    val_loss = val_loss / size
+    val_accuracy = 100. * correct / total
+
+    print(f'Validation Loss: {val_loss:.6f} Acc: {val_accuracy:.2f}%')
+    return val_accuracy
