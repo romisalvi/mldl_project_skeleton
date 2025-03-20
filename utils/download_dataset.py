075b89b9f6ae0833423d0a042cf15a2c85791c3d
rsalvi100 <162099174+romisalvi@users.noreply.github.com>
2025-03-20 12:32:26 +0100

    Update download_dataset.py

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -6,8 +6,6 @@ subprocess.run(["wget", "http://cs231n.stanford.edu/tiny-imagenet-200.zip"], che
 # Unzip the dataset
 subprocess.run(["unzip", "tiny-imagenet-200.zip", "-d", "tiny-imagenet"], check=True)
 
-!wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
-!unzip tiny-imagenet-200.zip -d tiny-imagenet
 with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
     for line in f:
         fn, cls, *_ = line.split('\t')
